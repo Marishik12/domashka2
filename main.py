405baec371e59f992b022c8dd99895a8547fc323
Marishik12 <koretskamaryna10@gmail.com>
2023-09-15 21:30:58 +0300

    commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import random
+class mathoperation:
+    def __init__(self, x, y):
+        self.__x = x
+        self.__y = y
+        self.__result = None
+    def __perform_operation(self):
+        operation = random.choice([
+        operation =
+        operation
+        'add', 'multiply'
+        ])
+        if operation == 'add':
+            self.__result = self.__x + self.__y
+            self
+        elif operation == 'multiply':
+            self.__result = self.__x * self.__y
+            self.__result = self.__x * self.__y
+            self.__result = self.__x * self
+            self.__result = self.__x
+            self.__result = self
+            self.__result
+    def get_result(self):
+        if self.__result is None:
+            self.__perform_operation()
+            self.__perform_operation()
+            self.__perform_operation
+            self
+            return self.__result
+def __str__(self):
+        x = 5
+        y = 7
+        math_operation = mathoperation(x, y)
+        math_operation = mathoperation(x, y)
+        math_operation = mathoperation(x)
+        math_operation = mathoperation
+print(math_operation)
